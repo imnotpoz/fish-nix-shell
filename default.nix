71298fabbf7e946956598b12e225babc370575d2
poz <poz@poz.pet>
2025-12-26 23:44:41 +0100

    update meta

--- default.nix
+++ default.nix
@@ -10,7 +10,10 @@
     wrapProgram $out/bin/fish-nix-shell-wrapper --prefix PATH ":" ${fish}/bin
     wrapProgram $out/bin/nix-shell-info
   '';
-  meta.description = "fish support for the nix-shell environment of the Nix package manager.";
-  meta.license = "MIT";
-  meta.homepage = "https://github.com/MrcJkb/fish-nix-shell";
+  meta = {
+    description = "fish support for the nix-shell environment of the Nix package manager.";
+    license = [ lib.licenses.mit ];
+    homepage = "https://github.com/imnotpoz/fish-nix-shell";
+    mainProgram = "fish-nix-shell";
+  };
 }
